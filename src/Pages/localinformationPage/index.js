75dc4d4c1c9c0a4f759bbd4f70ef5a4613b15199
jjijju0918 <jisu7032@naver.com>
2022-09-30 16:50:20 +0900

    관광지정보작업중

---
+++ src/Pages/localinformationPage/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import * as Styles from './style';
+import { MarginTopWrapper } from "../../Common/style";
+
+const Information = () => {
+    return(
+        <MarginTopWrapper margin>
+            <Styles.Contentbook>
+                <Styles.Title>
+                    관광지이름
+                </Styles.Title>
+            </Styles.Contentbook>
+        </MarginTopWrapper>
+
+    );
+}
+export default Information;
