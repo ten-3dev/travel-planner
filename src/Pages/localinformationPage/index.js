eee6129dae89e41f12cfcef7a21d260467db2643
jjijju0918 <jisu7032@naver.com>
2022-10-01 00:11:42 +0900

    관광지정보작업중

--- src/Pages/localinformationPage/index.js
+++ src/Pages/localinformationPage/index.js
@@ -9,8 +9,23 @@ const Information = () => {
                 <Styles.Title>
                     관광지이름(임시)
                 </Styles.Title>
+                <Styles.Hrr>
+                    <Styles.Img src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img>
+                </Styles.Hrr>
                 <Styles.Btn>+찜하기</Styles.Btn>
                 <Styles.ColorBar></Styles.ColorBar>
+                <Styles.Hee>
+                    <Styles.Baka src={process.env.PUBLIC_URL + "assets/baka.png"}></Styles.Baka>
+                </Styles.Hee>
+                <Styles.Mating>
+                    상세정보
+                </Styles.Mating>
+                <Styles.ColorBar1></Styles.ColorBar1>
+                <Styles.MMatin>
+                제 1 관광명소 남산서울타워
+                국내외 관광객들이 년 1,200만 명 방문하는 서울 제1의 관광명소인 남산서울타워
+                최근 한류 바람을 몰고 각종 예능, 드라마의 촬영지로 등등
+                </Styles.MMatin>
             </Styles.Contentbook>
         </MarginTopWrapper>
 
--- src/Pages/localinformationPage/style.js
+++ src/Pages/localinformationPage/style.js
@@ -12,16 +12,36 @@ export const Title = styled.div`
     font-weight: 500;
     margin-bottom: 40px;
     text-align: center;
+    font-weight: bold;
+`
+export const Mating = styled.div`
+    font-size: 20px;
+    margin-bottom: 20px;
+    margin-right:54%;
+    font-weight: bold;
+`
+export const MMatin = styled.div`
+    font-size: 20px;
+    margin-bottom: 20px;
+    margin-left: 20%;
+    //text-align: center;
+`
+export const ColorBar1 = styled.div`
+    width: 600px;
+    height: 3px;
+    margin-bottom: 3%;
+    margin-right: 22%;
+    background-color: grey ;
+    opacity: 0.2;
 `
 export const ColorBar = styled.div` //크기자동조절 찾기
     width: 900px;
     height: 3px;
     margin-top: 1%;
-    margin-bottom: 10%;
+    margin-bottom: 3%;
     background-color: grey ;
     opacity: 0.2;
 `
-
 export const Input = styled.input` //댓글사용
     border: 0;
     flex: 1;
@@ -29,12 +49,28 @@ export const Input = styled.input` //댓글사용
     background-color: white ;
     margin-left: 70%;
 `
-
 export const Btn = styled.button`
     border: 0;
     flex: 1;
+    width: 5%;
     background: rgba(0, 150, 100, 0.5);
     color: white;
     margin-left: 50%;
-    border-radius: 5px;
+    border-radius: 10px;
+`
+export const Hrr = styled.div`
+    width: 50px;
+    height: 10px;
+    margin-left: 40%;
+`
+export const Hee = styled.div`
+    //height: 400px;
+    //margin-left: 50%;
+   margin-bottom: 3%;
+`
+export const Img = styled.img`
+    width: 35px;
+`
+export const Baka = styled.img`
+    width: 950px;
 `
