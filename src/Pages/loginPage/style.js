a00edcbbefb7e9f2a3eb155bb155cdbfa59b52b2
jd <bamiyami@gmail.com>
2022-09-30 16:49:33 +0900

    login and signup pages

--- src/Pages/loginPage/style.js
+++ src/Pages/loginPage/style.js
@@ -26,13 +26,33 @@ export const LoginText = styled.text`
     position: relative;
 `
 
+export const LoginText2 = styled.text`
+    font-size: 18px;
+    font-weight: 500;
+    color: black;
+    margin-bottom: 40px;
+    position: relative;
+`
+
 export const InputBox = styled.div`
-    width: 200px;
-    height: 48px;
+    width: 350px;
+    height: 35px;
     background-color: white;
     border-radius: 10px;
     box-sizing: border-box;
     padding: 0 0 0 7px;
     display: flex;
     background-color: #F3F3F3;
+`
+
+export const KakaoBtn = styled.div`
+    width: 350px;
+    height: 35px;
+    background-color: #F6EF3D;
+    margin-top: 5px;
+    color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
 `
