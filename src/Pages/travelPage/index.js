c325ac14e519057fc67976493e20398aa2984e8e
mouse <gkdlfn032@gmail.com>
2022-10-01 16:41:58 +0900

    여행 리스트 작업중입니다.(석)

--- src/Pages/travelPage/index.js
+++ src/Pages/travelPage/index.js
@@ -15,38 +15,64 @@ const travelPage = () => {
                 <Styles.TravelWrapper2>
                     <Styles.Bar2/>
                     <Styles.TravelWrapper>
-                        <Styles.Img src={process.env.PUBLIC_URL + `assets/logo.png`}/>
-                        <Styles.Btn>+찜하기</Styles.Btn>
+                        <Styles.TravelWrapper5>
+                            <Styles.Img src={process.env.PUBLIC_URL + `assets/image32.png`}/>
+                            <Styles.Text1>
+                                제 1 관광명소 남산서울타워
+                                국내외 관광객들이 년 1,200만 명 방문하는 서울 제1의 관광명소인 남산서울타워
+                                최근 한류 바람을 몰고 각종 예능, 드라마의 촬영지로 등등
+                            </Styles.Text1>
+                        </Styles.TravelWrapper5>
+                        <Styles.BtnBox>
+                            <Styles.Img1 src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img1>
+                            <Styles.Btn>+찜하기</Styles.Btn>
+                        </Styles.BtnBox>  
                     </Styles.TravelWrapper>
                     <Styles.Bar2/>
                     <Styles.Bar2/>
                     <Styles.TravelWrapper>
                         <Styles.Img src={process.env.PUBLIC_URL + `assets/logo.png`}/>
-                        <Styles.Btn>+찜하기</Styles.Btn>
+                        <Styles.BtnBox>
+                            <Styles.Img1 src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img1>
+                            <Styles.Btn>+찜하기</Styles.Btn>
+                        </Styles.BtnBox>
                     </Styles.TravelWrapper>
                     <Styles.Bar2/>
                     <Styles.Bar2/>
                     <Styles.TravelWrapper>
                         <Styles.Img src={process.env.PUBLIC_URL + `assets/logo.png`}/>
-                        <Styles.Btn>+찜하기</Styles.Btn>
+                        <Styles.BtnBox>
+                            <Styles.Img1 src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img1>
+                            <Styles.Btn>+찜하기</Styles.Btn>
+                        </Styles.BtnBox>
                     </Styles.TravelWrapper>
                     <Styles.Bar2/>
                     <Styles.Bar2/>
                     <Styles.TravelWrapper>
                         <Styles.Img src={process.env.PUBLIC_URL + `assets/logo.png`}/>
-                        <Styles.Btn>+찜하기</Styles.Btn>
+                        <Styles.BtnBox>
+                            <Styles.Img1 src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img1>
+                            <Styles.Btn>+찜하기</Styles.Btn>
+                        </Styles.BtnBox>
                     </Styles.TravelWrapper>
                     <Styles.Bar2/>
                     <Styles.Bar2/>
                     <Styles.TravelWrapper>
                         <Styles.Img src={process.env.PUBLIC_URL + `assets/logo.png`}/>
-                        <Styles.Btn>+찜하기</Styles.Btn>
+                        <Styles.BtnBox>
+                            <Styles.Img1 src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img1>
+                            <Styles.Btn>+찜하기</Styles.Btn>
+                        </Styles.BtnBox>
                     </Styles.TravelWrapper>
                     <Styles.Bar2/>
                 </Styles.TravelWrapper2>
-                <Styles.TravelWrapper3>
+                <Styles.TravelWrapper4>
+                    <Styles.Text>필터</Styles.Text>
+                    <Styles.TravelWrapper3>
                     
-                </Styles.TravelWrapper3>
+                    </Styles.TravelWrapper3>
+                </Styles.TravelWrapper4>
+                
             </Styles.ContentBox>
 
         </MarginTopWrapper>
--- src/Pages/travelPage/style.js
+++ src/Pages/travelPage/style.js
@@ -8,13 +8,14 @@ export const Bar = styled.div`
     margin-bottom: 2%;
     background-color: black ;
 `
+
 export const Bar2 = styled.div`
     width: 1000px;
     height: 1px;
     position: sticky;
     left: 10px;
     margin-bottom: 1.5%;
-    margin-top: 3px;
+    margin-top: 7px;
     background-color: gray ;
     opacity: 0.2;
 `
@@ -50,22 +51,44 @@ export const Input = styled.input`
 `
 
 export const TravelWrapper = styled.div`
-    width: 1000px;
+    width: 100%;
     height: 125px; 
     padding: 0 0 0 7px;  
 `
 
 export const TravelWrapper2 = styled.div`
-    width: 1100px;
-    height: 850px; 
+    width: 100%;
     padding: 0 0 0 7px;  
 `
 
 export const TravelWrapper3 = styled.div`
+    width: 70%;
+    height: 80%; 
+    background-color: #00966480;
+    margin: 30px 50px 150px 90px;
+    
+`
+export const TravelWrapper4 = styled.div`
     width: 100%;
-    height: 850px; 
-    padding: 0 0 0 7px;  
-    background-color: red;
+    margin-top: 75px;   
+`
+
+export const TravelWrapper5 = styled.div`
+    width: 100%;  
+`
+export const TravelWrapper6 = styled.div`
+    width: 75%;  
+`
+
+export const Text = styled.div`
+    width: 100%; 
+    font-size: 30px;
+    text-align: center;
+`
+export const Text1 = styled.div`
+    width: 100%; 
+    text-align: center;
+    display: flex;
 `
 
 export const Img = styled.img`
@@ -73,6 +96,12 @@ export const Img = styled.img`
     width: 230px;
     height: 100px;
     margin-right: 15px;
+    margin-top: 5px;
+`
+
+export const Img1 = styled.img`
+    width: 20px;
+    height: 20px;
 `
 
 export const Tag = styled.div`
@@ -84,10 +113,19 @@ export const Tag = styled.div`
 `
 
 export const Btn = styled.span`
-    cursor: pointer;
-    background-color: #00966480;
+    border: 0;
+    width: 70px;
+    background: rgba(0, 150, 100, 0.5);
     color: white;
-    margin-left: 93%;
-    font-size: 16px;
-    border-radius: 5px;
+    border-radius: 10px;
+    margin-left: 10px;
+    font-size: 13px;
+    text-align: center;
+    cursor: pointer;
+`
+export const BtnBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
 `
