f4eae67623be23894377e80b52b6038a0747b72c
Daeyoung Gu <daeyounggu@Daeyoungui-MacBookAir.local>
2022-10-01 10:16:51 +0900

    일정상세페이지 하는중

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Header from "./Components/header";
 import Footer from "./Components/footer";
-import MainPage from "./Pages/mainPage";
-import LoginPage from "./Pages/loginPage";
-import SignPage from "./Pages/signPage"
+import CalendarPage from "./Pages/CalendarPage";
 import NotFoundPage from './Pages/notFoundPage';
 import Information from "./Pages/localinformationPage";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -11,11 +9,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
     <BrowserRouter>
-      <Header main/>
+      <Header/>
       <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/sign" element={<SignPage/>}/>
+        <Route path="/" element={<CalendarPage/>}/>
         <Route path="*" element={<NotFoundPage/>}/>
         <Route path="/Information" element={<Information/>}/>
       </Routes>
