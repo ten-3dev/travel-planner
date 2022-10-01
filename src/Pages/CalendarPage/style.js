c0a6fa95ea790b099104cd69f039d2203ace7032
gdy <ocstar9@gmail.com>
2022-10-01 12:52:39 +0900

    일정상세페이지ing

--- src/Pages/CalendarPage/style.js
+++ src/Pages/CalendarPage/style.js
@@ -4,53 +4,74 @@ export const Wrapper = styled.div`
     position: relative;
     display: flex;
     justify-content: center;
+    overflow: auto;
+
     height: 100vh;
     width: 100%;
-    overflow: hidden;
     
 `
 
 export const ContentBox = styled.div`
-    width: 1200px;
+    border: 5px solid green;
+    
     position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-item: center; 
+    overflow: auto;
+
+    width: 1200px;
     top: 50%;
     left: 50%;
     margin-left: -600px;
-    margin-top: -150px;
-    display: flex;
-    align-items: center;
+    
 `
 
 export const ImageBox = styled.div`
-    display: block;
-    align-content: center;
-    overflow: hidden;
+    border : 1px solid red;
+
+    position: relative;
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    height:700px;
+    margin-bottom:200px;
+     
+    
 `
 
 export const Image = styled.img`
     position: absolute;
+
     width: 100%;
-    top: 50%;
-    margin-top: -150px;
-    display: blockㄷ;
-    justify-content: center;
-    
-    
+
+
 `
 
 export const ShareBtn = styled.div`
+
     position: absolute;
+
     width: 100px;
-    height: 50px;
+    height: 30px;
+    right:0;
+    margin-top: 650px;
+    
     border-radius: 5px;
-    background-color: red;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    background-color: #ABE4C7; 
+
+
 
 `
 
 export const Menu = styled.div`
+    border: 1px solid blue;
+
+    position: absolute;
+    
+    width: 100%;
+    height: 100px;
 
 
 `
@@ -65,7 +86,13 @@ export const Map = styled.div`
 `
 
 export const Comment = styled.div`
+    position: absolute;
 
+    border: 1px solid yellow;
+
+    width: 100%;
+    height: 200px;
+    
 `
 
 export const ReviewBox = styled.div`
