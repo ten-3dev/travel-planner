e550a283784b686f385fcaedc11647e64fbe5c6c
jjijju0918 <jisu7032@naver.com>
2022-10-01 17:37:18 +0900

    관광지정보

--- src/Pages/localinformationPage/index.js
+++ src/Pages/localinformationPage/index.js
@@ -5,49 +5,57 @@ import { MarginTopWrapper } from "../../Common/style";
 const InformationPage = () => {
     return(
         <MarginTopWrapper margin>
-            <Styles.Contentbook>
-                <Styles.Title>
+
+            <Styles.Title>
                     관광지이름(임시)
                 </Styles.Title>
-                <Styles.BtnBox>
-                    <Styles.Img1 src={process.env.PUBLIC_URL + "assets/hrr.png"}></Styles.Img1>
-                    <Styles.Btn>+찜하기</Styles.Btn>
-                </Styles.BtnBox>
-                <Styles.ColorBar2></Styles.ColorBar2>
-                <Styles.Hrr2>
-                    <Styles.Img2 src={process.env.PUBLIC_URL + "assets/baka.png"}></Styles.Img2>
-                </Styles.Hrr2>
-                <Styles.Cont>
-                    <Styles.Infor1>
-                    상세정보
-                    </Styles.Infor1>
-                    <Styles.ColorBar1></Styles.ColorBar1>
-                    <Styles.Infor2>
+                <Styles.LikeBox>
+                    <Styles.Img1 src={"assets/hrr.png"}></Styles.Img1>
+                    <Styles.Like>+찜하기</Styles.Like>
+                </Styles.LikeBox>
+                <Styles.TopBar></Styles.TopBar>
+                <Styles.TitleImgBox>
+                    <Styles.Titleimage src={"assets/baka.png"}></Styles.Titleimage>
+                </Styles.TitleImgBox>
+                <Styles.InformationBox>
+                    <Styles.InformationTitle>
+                        상세정보
+                    </Styles.InformationTitle>
+                    <Styles.InformationBar></Styles.InformationBar>
+                    <Styles.InformationContnet>
                     제 1 관광명소 남산서울타워
                     국내외 관광객들이 년 1,200만 명 방문하는 서울 제1의 관광명소인 남산서울타워
                     최근 한류 바람을 몰고 각종 예능, 드라마의 촬영지로 등등
-                    </Styles.Infor2>
-                    <Styles.Infor3>
+                    </Styles.InformationContnet>
+                    <Styles.Sources>
                     [출처: 쮸짱이의 브이로그 히힛]
-                    </Styles.Infor3>
-                    <Styles.Hrr3>
-                        <Styles.Img3 src={process.env.PUBLIC_URL + "assets/임시지도.png"} ></Styles.Img3>
-                    </Styles.Hrr3>
-                    <Styles.Hrr4>
-                        <Styles.Img4 src={process.env.PUBLIC_URL + "assets/임시내용.png"}></Styles.Img4>
-                    </Styles.Hrr4>
-                    <Styles.Infor4>톡톡</Styles.Infor4>
-                    <Styles.ColorBar1></Styles.ColorBar1>
-                    <Styles.Cont2>
-                        <Styles.CoBar3></Styles.CoBar3>
-                        <Styles.Infor5>응원메시지</Styles.Infor5>
-                        <Styles.Im5 src={process.env.PUBLIC_URL + "assets/임시프로필사진.png"} ></Styles.Im5>
-                        <Styles.Input placeholder="소중한댓글을 남겨주세요."></Styles.Input>
-                        <Styles.Btn2>글쓰기</Styles.Btn2>
-                    </Styles.Cont2>
-                </Styles.Cont>
-        
-            </Styles.Contentbook>
+                    </Styles.Sources>
+                    <Styles.Map>지도</Styles.Map>
+                    <Styles.DetailedInfor>
+                        정보내용적기
+                    </Styles.DetailedInfor>
+                    <Styles.Talk>톡톡</Styles.Talk>
+                    <Styles.InformationBar />
+                    <Styles.TalkMessageBox>
+                        <Styles.MessageCommentsBox>
+                            <Styles.MessageCommentBox>
+                                    <Styles.MessageCommentIcon />
+                                    <Styles.Comment>데이트코스로 너무 좋아요!</Styles.Comment>
+                                    <Styles.Days>2022.06.24</Styles.Days>
+                            </Styles.MessageCommentBox>
+                        </Styles.MessageCommentsBox>
+                        <Styles.MymessageBox>
+                            <Styles.Reviewcomment>
+                                <Styles.Msg>리뷰쓰기</Styles.Msg>
+                            </Styles.Reviewcomment>
+                            <Styles.MyWriting>
+                                <Styles.Profile src={"assets/임시프로필사진.png"} ></Styles.Profile>
+                                <Styles.MessageInput placeholder="소중한댓글을 남겨주세요."></Styles.MessageInput>
+                                <Styles.MessageBtn>글쓰기</Styles.MessageBtn>
+                            </Styles.MyWriting>
+                        </Styles.MymessageBox>
+                    </Styles.TalkMessageBox>
+            </Styles.InformationBox>
         </MarginTopWrapper>
 
     );
--- src/Pages/localinformationPage/style.js
+++ src/Pages/localinformationPage/style.js
@@ -1,11 +1,5 @@
 import styled from "styled-components";
 
-export const Contentbook = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 180px
-`
 export const Title = styled.div`
     font-size: 30px;
     font-weight: 500;
@@ -13,94 +7,13 @@ export const Title = styled.div`
     text-align: center;
     font-weight: bold;
 `
-export const Infor1 = styled.div`
-    font-size: 20px;
-    margin-bottom: 20px;
-    margin-right:87%;
-    font-weight: bold;
-`
-export const Cont = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 1000px;
-    width: 100%;
-`
-export const Cont2 = styled.div`
-    width: 950px;
-    height: 500px;
-    margin-top: 1%;
-    margin-bottom: 3%;
-    background-color: grey ;
-    opacity: 0.4;
-    border-radius: 20px;
-`
-export const Infor2 = styled.div`
-    font-size: 20px;
-    margin-bottom: 20px;
-    margin-left: 2.5%;
-`
-export const Infor3 = styled.div`
-    font-size: 15px;
-    margin-bottom: 20px;
-    margin-right:75%;
-    font-weight: bold;
-`
-export const Infor4 = styled.div`
-    font-size: 25px;
-    margin-bottom: 20px;
-    margin-right:87%;
-    font-weight: bold;
-`
-export const Infor5 = styled.div`
-    font-size: 10px;
-    margin-bottom: 1px;
-    margin-left:5%;
-    font-weight: bold;
-`
-export const ColorBar1 = styled.div`
-    width: 600px;
-    height: 3px;
-    margin-bottom: 3%;
-    margin-right: 35%;
-    background-color: grey ;
-    opacity: 0.2;
-`
-export const CoBar3 = styled.div`
-    width: 600px;
-    height: 3px;
-    margin-top:35%;
-    margin-left: 5%;
-    background-color:Black;
-`
-export const ColorBar2 = styled.div` //크기자동조절 찾기
-    width: 100%;
-    height: 3px;
-    margin-top: 1%;
-    margin-bottom: 3%;
-    background-color: grey ;
-    opacity: 0.2;
-`
-export const Input = styled.input` 
-    border: 0;
-    flex: 1;
-    width: 600px;
-    height: 100px;
-    font-size: 18px;
-    background-color: white ;
-    padding-left: 9px;
-    margin-left:15px;
-    margin-top:20px;
-`
-
-export const BtnBox = styled.div`
+export const LikeBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
     width: 100%;
 `
-
-export const Btn = styled.button`
+export const Like = styled.button`
     border: 0;
     width: 70px;
     background: rgba(0, 150, 100, 0.5);
@@ -109,45 +22,146 @@ export const Btn = styled.button`
     margin-left: 15px;
     cursor: pointer;
 `
-
-export const Btn2 = styled.button`
-    border: 0;
-    flex: 1;
-    width: 10%;
-    height: 20%;
-    background: rgba(0, 50, 100, 1);
-    color: white;
-    margin-left: 1%;
-    border-radius: 5px;
+export const TopBar = styled.div` //크기자동조절 찾기
+    width: 100%;
+    height: 3px;
+    margin-top: 1%;
+    margin-bottom: 3%;
+    background-color: grey ;
+    opacity: 0.2;
 `
-
-export const Hrr2 = styled.div`
+export const TitleImgBox = styled.div`
    margin-bottom: 3%;
 `
-export const Hrr3 = styled.div`
-    margin-bottom: 3%;
-`
-export const Hrr4 = styled.div`
-    margin-bottom: 3%;
-`
 
+export const InformationTitle = styled.div`
+    font-size: 20px;
+    margin-bottom: 20px;
+    font-weight: bold;
+`
+export const InformationBox = styled.div`
+    width: 100%;
+`
+export const TalkMessageBox = styled.div`
+    margin-bottom: 5%;
+    background-color: rgba(49, 49, 49, 0.5);
+    /* opacity: 0.5; */
+    border-radius: 20px;
+    box-sizing: border-box;
+    padding: 50px 70px;
+`
+export const InformationContnet = styled.div`
+    font-size: 20px;
+    margin-bottom: 20px;
+`
+export const Sources = styled.div`
+    font-size: 15px;
+    margin-bottom: 20px;
+    margin-right:75%;
+    font-weight: bold;
+`
+export const Talk = styled.div`
+    font-size: 25px;
+    //margin-bottom: 20px;
+    //margin-right:87%;
+    font-weight: bold;
+`
+export const Msg = styled.div`
+    font-size: 10px;
+    font-weight: bold;
+    margin-bottom: 10px;
+`
+export const InformationBar = styled.div`
+    width: 600px;
+    height: 3px;
+    margin-bottom: 1%;
+    margin-right: 35%;
+    background-color: grey ;
+    opacity: 0.2;
+`
+export const MessageBar = styled.div`
+    width: 100%;
+    height: 3px;
+    background-color: Black;
+`
+export const MessageInput = styled.textarea` 
+    border: 0;
+    flex: 1;
+    height: 100px;
+    font-size: 18px;
+    background-color: white ;
+    margin: 0 40px;
+`
+export const MessageCommentsBox = styled.div`
+    margin-bottom: 200px;
+`
+export const MessageCommentBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid black;
+    box-sizing: border-box;
+    padding: 15px 0;
+    width: 90%;
+`
+export const MessageCommentIcon = styled.div` // 나중에 img 로 바꿀꺼
+    width: 35px;
+    height: 35px;
+    border-radius: 17.5px;
+    background-color: blue;
+    margin-right: 180px;
+`
+export const Comment = styled.div`
+    font-size: 15px;
+    margin-bottom: 1%;
+    margin-top:1%; 
+    flex: 1;
+`
+export const Days = styled.div`
+    font-size: 15px;
+    font-weight: bold;
+`
+export const MessageBtn = styled.button`
+    border: 0;
+    width: 70px;
+    height: 30px;
+    background: rgba(0, 50, 100, 1);
+    color: white;
+    border-radius: 5px;
+    align-self: flex-end;
+    cursor: pointer;
+`
+export const Map = styled.div`
+    margin-bottom: 3%;
+    height:200px;
+    background-color:grey;
+`
+export const DetailedInfor = styled.div`
+    margin-bottom: 3%;
+    height:200px;
+    background-color:green;
+`
 export const Img1 = styled.img`
     width: 35px;
 `
-export const Img2 = styled.img`
-    width: 950px;
+export const Titleimage = styled.img`
+    width: 100%;
 `
-export const Img3 = styled.img`
-    width: 950px;
-    height: 200px;
+export const Profile = styled.img`
+    width: 35px;
+    height: 35px;
+    border-radius: 17.5px;
+    align-self: flex-start;
 `
-export const Img4 = styled.img`
-    width: 1000px;
-    height: 400px;
+export const MymessageBox = styled.div`
+    border-top: 1px solid black;
 `
-export const Im5 = styled.img`
-    width: 50px;
-    height: 50px;
-    margin-left: 6%;
-    border-radius: 20px;
+export const Reviewcomment = styled.div`
+    margin-bottom: 10px;
+`
+export const MyWriting = styled.div`
+    margin-bottom:40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `
