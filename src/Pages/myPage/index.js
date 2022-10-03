217cac67642472846582bf597cc44fc3dfb1e60b
맵쥐 <gkdlfn@gmail.com>
2022-10-03 23:38:07 +0900

    마이페이지 수정중

---
+++ src/Pages/myPage/index.js
@@ -0,0 +1,136 @@
+import React from "react";
+import * as Styles from './style';
+import { MarginTopWrapper } from "../../Common/style";
+
+const myPage = () => {
+    return (
+        <MarginTopWrapper margin>
+            <Styles.ProfileBox>
+                <Styles.SettingBox>
+                    <Styles.Profile src={`assets/임시프로필사진.png`}/>
+                    <Styles.Setting src={`assets/settings.png`}/>
+                </Styles.SettingBox>
+                <Styles.Text>tetrisGosu</Styles.Text>
+            </Styles.ProfileBox>
+            <Styles.MyWritingBox>
+                <Styles.Box>
+                    <Styles.Box1>내가 쓴 댓글</Styles.Box1>
+                    <Styles.Box1>3</Styles.Box1>
+                </Styles.Box>
+                <Styles.Box>
+                    <Styles.Box1>나의 플랜</Styles.Box1>
+                    <Styles.Box1>7</Styles.Box1>
+                </Styles.Box>
+                <Styles.Box2>
+                    <Styles.Box1>공유한 플랜</Styles.Box1>
+                    <Styles.Box1>2</Styles.Box1>
+                </Styles.Box2>
+            </Styles.MyWritingBox>
+            <Styles.CommentsBox2>
+                <Styles.MyComments>내가 쓴 댓글</Styles.MyComments>
+                <Styles.CommentsBox1>
+                    <Styles.Img src={`assets/plan_ex1.png`}/>
+                    <Styles.CommentsBox>
+                        <Styles.Text2>김광석 길(대구)</Styles.Text2>
+                        <Styles.Text1>볼거 1도 없음..가지마셈..</Styles.Text1>
+                        <Styles.Text2>★ ★ ★ ★</Styles.Text2>
+                    </Styles.CommentsBox>
+                    <Styles.Text3>1995-05-09</Styles.Text3>
+                </Styles.CommentsBox1>
+                <Styles.CommentsBox1>
+                    <Styles.Img src={`assets/plan_ex1.png`}/>
+                    <Styles.CommentsBox>
+                        <Styles.Text2>김광석 길(대구)</Styles.Text2>
+                        <Styles.Text1>볼거 1도 없음..가지마셈..</Styles.Text1>
+                        <Styles.Text2>★ ★ ★ ★</Styles.Text2>
+                    </Styles.CommentsBox>
+                    <Styles.Text3>1995-05-09</Styles.Text3>
+                </Styles.CommentsBox1>
+                <Styles.CommentsBox1>
+                    <Styles.Img src={`assets/plan_ex1.png`}/>
+                    <Styles.CommentsBox>
+                        <Styles.Text2>김광석 길(대구)</Styles.Text2>
+                        <Styles.Text1>볼거 1도 없음..가지마셈..</Styles.Text1>
+                        <Styles.Text2>★ ★ ★ ★</Styles.Text2>
+                    </Styles.CommentsBox>
+                    <Styles.Text3>1995-05-09</Styles.Text3>
+                </Styles.CommentsBox1>
+                <Styles.Text3> 1 2 3 4 </Styles.Text3>
+            </Styles.CommentsBox2>
+            <Styles.CommentsBox2>
+                <Styles.MyComments>나의 여행 일정</Styles.MyComments>
+                <Styles.CommentsBox1>
+                    <Styles.PlanImg src={`assets/image32.png`}/>
+                    <Styles.CommentsBox>
+                        <Styles.Text2>대구</Styles.Text2>
+                        <Styles.PlanText>풀네임: 외톨이 여행</Styles.PlanText>
+                        <Styles.ModifyDeleteBox>일정 수정</Styles.ModifyDeleteBox>
+                    </Styles.CommentsBox>
+                    <Styles.CommentsBox>
+                        <Styles.PeriodText>2022-10-03 ~ 2022-10-04</Styles.PeriodText>
+                        <Styles.ModifyDeleteBox>일정 삭제</Styles.ModifyDeleteBox>
+                    </Styles.CommentsBox>
+                </Styles.CommentsBox1>
+                <Styles.CommentsBox1>
+                    <Styles.PlanImg src={`assets/image32.png`}/>
+                    <Styles.CommentsBox>
+                        <Styles.Text2>대구</Styles.Text2>
+                        <Styles.PlanText>풀네임: 외톨이 여행</Styles.PlanText>
+                        <Styles.ModifyDeleteBox>일정 수정</Styles.ModifyDeleteBox>
+                    </Styles.CommentsBox>
+                    <Styles.CommentsBox>
+                        <Styles.PeriodText>2022-10-03 ~ 2022-10-04</Styles.PeriodText>
+                        <Styles.ModifyDeleteBox>일정 삭제</Styles.ModifyDeleteBox>
+                    </Styles.CommentsBox>
+                </Styles.CommentsBox1>
+                <Styles.CommentsBox1>
+                    <Styles.PlanImg src={`assets/image32.png`}/>
+                    <Styles.CommentsBox>
+                        <Styles.Text2>대구</Styles.Text2>
+                        <Styles.PlanText>풀네임: 외톨이 여행</Styles.PlanText>
+                        <Styles.ModifyDeleteBox>일정 수정</Styles.ModifyDeleteBox>
+                    </Styles.CommentsBox>
+                    <Styles.CommentsBox>
+                        <Styles.PeriodText>2022-10-03 ~ 2022-10-04</Styles.PeriodText>
+                        <Styles.ModifyDeleteBox>일정 삭제</Styles.ModifyDeleteBox>
+                    </Styles.CommentsBox>
+                </Styles.CommentsBox1>
+                <Styles.Text3> 1 2 3 4 </Styles.Text3>
+            </Styles.CommentsBox2>
+            <Styles.CommentsBox2>
+                <Styles.MyComments>나의 공유 플랜</Styles.MyComments>
+                <Styles.CommentsBox1>
+                    <Styles.PlanImg src={`assets/image32.png`}/>
+                    <Styles.MySharingPlanBox>
+                        <Styles.Box3>
+                            <Styles.Text4>서울 여행</Styles.Text4>
+                            <Styles.Text4>2022-09-24 ~ 2022-09-26</Styles.Text4> 
+                        </Styles.Box3>
+                        <Styles.Box4>
+                            <Styles.Img1 src={"assets/hrr.png"}/>
+                            <Styles.Text1>1</Styles.Text1>
+                            <Styles.Text5>By.JH</Styles.Text5>
+                        </Styles.Box4>
+                    </Styles.MySharingPlanBox>
+                </Styles.CommentsBox1>
+                <Styles.CommentsBox1>
+                    <Styles.PlanImg src={`assets/image32.png`}/>
+                    <Styles.MySharingPlanBox>
+                        <Styles.Box3>
+                            <Styles.Text4>서울 여행</Styles.Text4>
+                            <Styles.Text4>2022-09-24 ~ 2022-09-26</Styles.Text4> 
+                        </Styles.Box3>
+                        <Styles.Box4>
+                            <Styles.Img1 src={"assets/hrr.png"}/>
+                            <Styles.Text1>1</Styles.Text1>
+                            <Styles.Text5>By.JH</Styles.Text5>
+                        </Styles.Box4>
+                    </Styles.MySharingPlanBox>
+                </Styles.CommentsBox1>
+                <Styles.Text3> 1 2 3 4 </Styles.Text3>
+            </Styles.CommentsBox2>
+        </MarginTopWrapper>
+    )
+}
+
+export default myPage;
---
+++ src/Pages/myPage/style.js
@@ -0,0 +1,186 @@
+import styled from "styled-components";
+
+export const ProfileBox = styled.div`
+    width: 100%;
+    display: flex;
+    padding: 150px 0 50px 50px;
+`
+
+export const SettingBox = styled.div`
+    width: 22%;
+    display: flex-end;
+
+`
+export const MyWritingBox = styled.div`
+    width: 100%;
+    display: flex;
+    
+`
+
+export const CommentsBox = styled.div`
+    width: 100%;
+    margin-bottom: 20px;
+`
+
+export const CommentsBox1 = styled.div`
+    width: 100%;
+    margin-bottom: 50px;
+    margin-left: 130px;
+    display: flex;
+`
+export const CommentsBox2 = styled.div`
+width: 100%;
+margin-bottom: 20px;
+border-bottom: 1px solid #000000 ;
+margin-top: 50px;
+`
+
+export const MyComments = styled.div`
+    width: 18%;
+    margin-bottom: 20px;
+    font-size: 30px;
+    margin-left: 130px;
+    border-bottom: 1px solid #000000 ;
+    padding: 0 0 10px 0;
+    font-weight: bold ;
+    text-align : left;
+`
+
+export const Box = styled.div`
+    width: 100%;
+    border-bottom: 1px solid #000000 ;
+    border-right: 1px solid #000000;
+`
+
+export const Box1 = styled.div`
+    width: 100%;
+    font-size: 30px;
+    text-align: center;
+    margin-bottom: 30px;
+    cursor: pointer;
+`
+export const Box2 = styled.div`
+    width: 100%;
+    border-bottom: 1px solid #000000 ;
+`
+export const Box3 = styled.div`
+    width: 100%;
+    height: 120px;
+`
+
+export const Box4 = styled.div`
+    width: 100%;
+    display: flex;
+`
+
+export const ModifyDeleteBox = styled.div`
+    width: 30%;
+    height: 15%;
+    background-color: #A4A4A4;
+    align-items: center;
+    text-align: center;
+    font-size: 20px;
+    border-radius: 20px;
+    margin-top: 10px;
+    margin-left: 50px;
+    cursor: pointer;
+`
+export const MySharingPlanBox = styled.div`
+    width: 100%;
+    background-color: #FAF3E1;
+    border-radius: 30px;
+    margin: 20px 200px 20px 100px;
+    border: 1px solid #CECECD ;
+    padding-top: 20px;
+`
+
+export const Setting = styled.img`
+    cursor: pointer;
+    width: 60px;
+    height: 60px;
+    align-items:flex-end ;
+    justify-content: flex-start;
+    
+`
+
+export const Profile = styled.img`
+    width: 150px;
+    height: 150px;
+    border-radius: 100%;
+    margin-bottom: 50px;
+    cursor: pointer;
+`
+
+export const Text = styled.div`
+    width: 100%; 
+    height: 150px;
+    font-size: 30px;
+    margin: 100px 0 0 0;
+`
+export const Text1 = styled.div`
+    width: 100%; 
+    font-size: 20px;
+    margin-left: 7px;
+`
+
+export const Text2 = styled.div`
+    width: 100%; 
+    font-size: 30px;
+    font-weight: bold;
+`
+
+export const Text3 = styled.div`
+    width: 100%; 
+    font-size: 25px;
+    text-align: center;
+    margin-right: 100px;
+    margin-bottom: 50px;
+`
+
+export const Text4 = styled.div`
+    width: 90%; 
+    margin: 0 30px 0 30px;
+`
+
+export const Text5 = styled.div`
+    width: 90%; 
+    margin: 0 50px 0 30px;
+    text-align: right;
+`
+
+export const PlanText = styled.div`
+    width: 100%; 
+    height: 150px;
+    font-size: 20px;
+    margin-left: 5px;
+`
+
+export const PeriodText = styled.div`
+    width: 100%; 
+    height: 192px;
+    font-size: 20px;
+    margin-left: 5px;
+`
+export const Img = styled.img`
+    cursor: pointer;
+    width: 140px;
+    height: 140px;
+    margin-right: 15px;
+    margin-top: 5px;
+    border-radius: 10px;
+`
+export const PlanImg = styled.img`
+    cursor: pointer;
+    width: 250px;
+    height: 230px;
+    margin-right: 15px;
+    margin-top: 5px;
+    border-radius: 10px;
+`
+
+export const Img1 = styled.img`
+    width: 30px;
+    height: 30px;
+    margin: 0 0 0 30px;
+    display: flex;
+`
