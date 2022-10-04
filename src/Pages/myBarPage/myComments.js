fa30037998167a1a72f9e2537080c3b4916aa84f
맵쥐 <gkdlfn@gmail.com>
2022-10-05 03:08:03 +0900

    수정중

---
+++ src/Pages/myBarPage/myComments.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+  
+const MyComments = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '1000vh'
+      }}
+    >
+      <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+    </div>
+  );
+};
+  
+export default MyComments;
--- src/Pages/myPage/style.js
+++ src/Pages/myPage/style.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+
+import { NavLink as Link } from 'react-router-dom';
+
+
 export const ProfileBox = styled.div`
     width: 1000px;
     display: flex;
@@ -187,3 +191,81 @@ export const Img1 = styled.img`
     margin: 0 0 0 30px;
     display: flex;
 `
+
+
+
+
+
+
+export const Nav = styled.nav`
+  background: #63D471;
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  //padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
+  /* Third Nav */
+  /* justify-content: flex-start; */
+`;
+  
+export const NavLink = styled(Link)`
+  color: #000000;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: #808080;
+  padding: 10px 22px;
+  color: #000000;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  /* Second Nav */
+  margin-left: 24px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
+`;
+  
+  
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+  /* Second Nav */
+  /* margin-right: 24px; */
+  /* Third Nav */
+  /* width: 100vw;
+  white-space: nowrap; */
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+  
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  /* Third Nav */
+  /* justify-content: flex-end;
+  width: 100vw; */
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+  
