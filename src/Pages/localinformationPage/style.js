9cd9b2dea16023de85eccf4f41dea3052fab5ed9
jjijju0918 <jisu7032@naver.com>
2022-10-04 12:32:36 +0900

    관광지정보수정완료

--- src/Pages/localinformationPage/style.js
+++ src/Pages/localinformationPage/style.js
@@ -73,6 +73,7 @@ export const Msg = styled.div`
 export const InformationBar = styled.div`
     width: 600px;
     height: 3px;
+    margin-top: 10px;
     margin-bottom: 10px;
     margin-right: 35px;
     background-color: grey ;
@@ -138,8 +139,7 @@ export const Map = styled.div`
 `
 export const DetailedInforBox = styled.div`
     margin-bottom:30px;
-    //background-color:wheat;
-    align-items: center;
+    //align-items: center;
     display: flex;
 `
 export const DetaBox = styled.div`
@@ -149,13 +149,10 @@ export const DetaBoxrightBox = styled.div`
     width: 50%;
 ` 
 export const DetaFontBox = styled.div`
-    align-items: center;
     display: flex;
 `
 export const DetaFontrightBox = styled.div`
     display: flex;
-    align-self: flex-start;
-    margin-bottom: 22px;
 `
 export const DetaFont = styled.div`
     font-size: 20px;
@@ -198,7 +195,4 @@ export const MyWriting = styled.div`
     justify-content: space-between;
 `
 
-//상세정보 홈페이지 박스를 문의안내 줄 위치에 나오게 하기 임시방편해둠
-//DetaFontrightBox 안에  margin-bottom: 22px; 대칭 맞춰놨지만 수정해야할듯
-//칸이 넓혀서 맞춘거임 한번 컨펌받구 수정할거 해보기
 //페이지네이션 댓글 나의 글쓰기 밑에 만들기 라이브러리 사용예정
