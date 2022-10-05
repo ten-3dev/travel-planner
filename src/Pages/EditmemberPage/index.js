d31ab819fcd242bd002b4dff513bbb6a5c6b993c
jjijju0918 <jisu7032@naver.com>
2022-10-05 14:28:54 +0900

    회원정보수정ing

--- src/Pages/EditmemberPage/index.js
+++ src/Pages/EditmemberPage/index.js
@@ -7,6 +7,45 @@ export const EditmemberPage = () => {
         <MarginTopWrapper margin>
             <Styles.EditTitle>회원정보수정</Styles.EditTitle>
             <Styles.TitleBar/>
+            <Styles.MemberInforBox>
+                <Styles.MemberContentBox>
+                   <Styles.MemberEdit>아이디</Styles.MemberEdit>
+                   <Styles.MemberConrent placeholder="jjijju" ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>비밀번호</Styles.MemberEdit>
+                    <Styles.MemberConrent placeholder="비밀번호를 입력해주세요." ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>비밀번호확인</Styles.MemberEdit>
+                    <Styles.MemberConrent placeholder="비밀번호를 다시입력해주세요." ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>이름</Styles.MemberEdit>
+                    <Styles.MemberConrent placeholder="김찌쭈" ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>이메일</Styles.MemberEdit>
+                    <Styles.MemberConrent placeholder="su70322@naver.com" ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>연락처</Styles.MemberEdit>
+                    <Styles.MemberConrent placeholder="010-5899-7032" ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>생년월일</Styles.MemberEdit>
+                    <Styles.MemberConrent placeholder="1997-09-18" ></Styles.MemberConrent>
+                </Styles.MemberContentBox>
+                <Styles.MemberContentBox>
+                    <Styles.MemberEdit>성별</Styles.MemberEdit>
+                   <Styles.GenderCheck> ○ 남 ● 여</Styles.GenderCheck>
+                </Styles.MemberContentBox>
+            </Styles.MemberInforBox>
+            <Styles.BtnBox>
+                <Styles.Btn>수정하기</Styles.Btn>
+                <Styles.Btn>이전</Styles.Btn>
+                <Styles.DeleteBtn>탈퇴하기</Styles.DeleteBtn>
+            </Styles.BtnBox>
         </MarginTopWrapper>
     );
 }
--- src/Pages/EditmemberPage/style.js
+++ src/Pages/EditmemberPage/style.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const EditTitle = styled.div`
-    font-size: 30px;
+    font-size: 40px;
     font-weight: 500px;
-    margin-bottom: 10px;
+    margin-bottom: 15px;
     margin-top: 200px;
     text-align: center;
     font-weight: bold;
@@ -12,7 +12,65 @@ export const TitleBar = styled.div`
     width: 100%;
     height: 3px;
     margin-top: 10px;
-    margin-bottom: 30px;
+    margin-bottom: 100px;
     background-color: black ;
     //opacity: 0.2;
+`
+export const MemberInforBox = styled.div`
+    width: 100%;
+`
+export const MemberContentBox = styled.div`
+    margin: 40px 40px 100px;
+    width: 900px;
+    display: flex;
+`
+export const MemberEdit = styled.div`
+    width: 200px;
+    font-size: 25px;
+    font-weight: bold;
+`
+export const MemberConrent = styled.input` //칸박스 수정해야함 rgba로 변경하고 아이디~비번까지는 글씨 투명 나머지는 글씨 잘보이게 
+    width: 400px;
+    height: 50px;
+    font-size: 25px;
+    text-align: center; 
+    cursor: pointer;
+    border-radius: 9px;
+    background-color: #F5F5F5;
+
+`
+export const GenderCheck = styled.div` //성별버튼 만들어야함
+    width: 400px;
+    font-size: 25px;
+    cursor: pointer;
+    border-radius: 12px;
+`
+export const Btn = styled.button`
+    border: 0;
+    width: 150px;
+    height: 40px;
+    margin: 30px 10px;
+    font-size: 25px;
+    background: rgba(0, 100, 100, 0.5);
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+`
+export const DeleteBtn = styled.button`
+    border: 0;
+    width: 150px;
+    height: 40px;
+    margin: 30px 10px;
+    font-size: 25px;
+    background-color: red;
+    //background: rgba(0, 50, 100, 1);
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+`
+export const BtnBox = styled.div`
+    width: 400px;
+    margin: 30px 400px;
+    width: 900px;
+    display: flex;
 `
