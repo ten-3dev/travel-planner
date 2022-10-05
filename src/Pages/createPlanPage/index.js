ac0e0ec99ac61408066aed211425e9d1101b3484
ten-3dev <mj03dev@gmail.com>
2022-10-05 17:51:38 +0900

    캘린더 작업ing

--- src/Pages/createPlanPage/index.js
+++ src/Pages/createPlanPage/index.js
@@ -2,15 +2,44 @@ import React, { useState, useContext, useEffect } from "react";
 import * as Styles from './style';
 import { UserContext } from "../../Store/users";
 
+const CreatePlanCalendar = ({open, setOpen, setDateList}) => {
+    const [value, onChange] = useState(new Date());
 
-const CreatePlanCalendar = () => {
+    const getApply = () => {
+        // 날짜 사이 일 수 구함
+        const diffDate = value[0].getTime() - value[1].getTime();
+        const dateN = Math.ceil(Math.abs(diffDate / (1000 * 60 * 60 * 24)));
+
+        const dateArr = [(value[0])];
+
+        // 반복문을 이용해 내일을 구하고 또 내일내일을 구하는 식으로 리스트를 만듦
+        for(let i = 0; i < dateN - 1; i++){
+            const date = dateArr[dateArr.length - 1];
+            const tomorrow = new Date(date);
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            
+            dateArr.push(tomorrow);
+        }
+        
+        setDateList(dateArr);
+        setOpen(false);
+    }
+
     return(
-        <>adfasdfasdfsadf</>
+        <Styles.ModalCustom isOpen={open} style={{overlay: {zIndex: "1"}}} ariaHideApp={false}>
+            <Styles.CalendarCustom onChange={onChange} value={value} selectRange />
+            <Styles.BtnBox>
+                <Styles.Btn onClick={() => setOpen(false)}>이전</Styles.Btn>
+                <Styles.Btn onClick={() => getApply()}>적용하기</Styles.Btn>
+            </Styles.BtnBox>
+        </Styles.ModalCustom>
     )
 }
 
 const CreatePlanPage = () => {
     const [isModelOpen, setIsModelOpen] = useState(true); //로그인용 모달
+    const [dateList, setDateList] = useState();
+
     const context = useContext(UserContext);
     const { setLayoutOpen } = context;
 
@@ -18,11 +47,14 @@ const CreatePlanPage = () => {
         setLayoutOpen(false);
     });
 
+    useEffect(() => {
+        console.log(dateList);
+    }, [dateList])
+
     return(
-        <Styles.ModalCustom isOpen={isModelOpen} style={{overlay: {zIndex: "1"}}} ariaHideApp={false}>
-            <CreatePlanCalendar/>
-            <button onClick={() => setIsModelOpen(!isModelOpen)}>닫기</button>
-        </Styles.ModalCustom>
+        <>
+            <CreatePlanCalendar open={isModelOpen} setOpen={setIsModelOpen} setDateList={setDateList}/>
+        </>
     )
 }
 
--- src/Pages/createPlanPage/style.js
+++ src/Pages/createPlanPage/style.js
@@ -1,14 +1,93 @@
 import styled from "styled-components";
 import Modal from 'react-modal';
+import Calendar from 'react-calendar';
 
 export const ModalCustom = styled(Modal)`
-    width: 300px;
-    height: 300px;
-    background-color: black;
+    width: 600px;
+    background-color: white;
+    border: 1px solid gray;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    border: 0;
     outline: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const BtnBox = styled.div`
+    width: 100%;
+    border-top: 1px solid gray;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    box-sizing: border-box;
+    padding: 0 25px;
+`
+
+export const Btn = styled.div`
+    display: inline-block;
+    box-sizing: border-box;
+    padding: 3px 5px;
+    height: 30px;
+    border: 1px solid rgba(95, 95, 95, 0.5);
+    font-size: 13px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 15px;
+    cursor: pointer;
+`
+
+export const CalendarCustom = styled(Calendar)`
+    width: 100%;
+    background-color: white;
+    margin-bottom: 100px;
+    /* ~~~ container styles ~~~ */
+    .react-calendar__navigation {
+        display: flex;
+        .react-calendar__navigation__label {
+            font-weight: bold;
+        }
+        .react-calendar__navigation__arrow {
+            flex-grow: 0.333;
+        }
+    }
+    /* ~~~ label styles ~~~ */
+    .react-calendar__month-view__weekdays {
+        text-align: center;
+    }
+    /* ~~~ button styles ~~~ */
+    button {
+        background-color: white;
+        border: 0;
+        color: black;
+        padding: 8px 0;
+        &:hover {
+            background-color: #D6D6D6;
+        }
+    }
+    /* ~~~ day grid styles ~~~ */
+    .react-calendar__month-view__days {
+        display: grid !important;
+        grid-template-columns: 14.2% 14.2% 14.2% 14.2% 14.2% 14.2% 14.2%; 
+
+        .react-calendar__tile--range {
+            background-color: #9F9F9F;
+            color: white;
+        }
+    }
+    /* ~~~ other view styles ~~~ */
+  .react-calendar__year-view__months, .react-calendar__decade-view__years, .react-calendar__century-view__decades {
+    display: grid !important;
+    grid-template-columns: 20% 20% 20% 20% 20%;
+    &.react-calendar__year-view__months {
+      grid-template-columns: 33.3% 33.3% 33.3%;
+    }
+  }
+  abbr{
+    text-decoration: none;
+  }
 `
