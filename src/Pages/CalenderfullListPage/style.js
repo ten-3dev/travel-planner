e554278d8146eea62d1ae8928693b19526854988
jjijju0918 <jisu7032@naver.com>
2022-10-05 18:04:02 +0900

    달력리스트

---
+++ src/Pages/CalenderfullListPage/style.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+//이름 바꾸고 내용 다 바꿔야함
+
+export const AllBox = styled.div`
+    width: 100%;
+    margin-top: 300px;
+    margin-bottom: 500px;
+`
+export const MapBox = styled.div`
+    width: 900px;
+    margin-left: 300px;
+`
+export const MapImg = styled.div`
+    height: 1000px;
+    background-color: yellow;
+`
+export const CalenderDay = styled.div`
+    font-size: 20px;
+    font-weight: 500px;
+    font-weight: bold;  
+`
+export const CalenderImg = styled.img`
+    width: 100px;
+    margin: 0 30px;
+    cursor: pointer;
+`
+export const ContentBox = styled.div`
+   // align-items: center;
+   display: flex;
+`
+export const ContentImg = styled.img`
+    width: 50px;
+    cursor: pointer;
+`
+export const ContentTitle = styled.div`
+    font-size: 20px;
+    font-weight: bold; 
+`
+export const ContentText = styled.div`
+    font-size: 15px;
+   // flex-direction: row;
+    //font-weight: bold; 
+`
+export const ContentTextBox = styled.div`
+
+`
+    
+export const calenderBox = styled.div`
+    display: flex;
+   // align-items: center;
+`
+export const ScheduleBox = styled.div`
+    //display: flex;
+    width: 700px;
+    margin: 50px 50px 50px;
+`
+export const ScheduleContentBox = styled.div`
+    width: 500px;
+    margin-top: 30px;
+    font-size: 20px;
+    font-weight: 500px;
+    text-align: center;
+    font-weight: bold;  
+`
+export const ScheduleDayBox = styled.div`
+    width: 500px;
+    margin-top: 30px;
+    font-size: 35px;
+    font-weight: 500px;
+    text-align: center;
+    font-weight: bold;
+`
+export const ScheduleBtnBox = styled.button`
+    width: 150px;
+    background: rgba(0, 150, 100, 0.5);
+    border-radius: 5px;
+    margin: 30px 170px;
+    cursor: pointer;
+    color: white;
+`
+export const TitleInput = styled.input`
+    width: 300px;
+    margin-top: 30px;
+    margin-left: 100px;
+    font-size: 20px;
+    //font-weight: 500px;
+    text-align: center;
+    //font-weight: bold;
+`
