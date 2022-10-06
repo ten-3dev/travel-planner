31d47016306f13ade210741356fa61f71467bc77
맵쥐 <gkdlfn@gmail.com>
2022-10-06 22:25:12 +0900

    비밀번호 확인페이지 생성

---
+++ src/Pages/passwordCheckPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as Styles from './style';
+import { MarginTopWrapper } from "../../Common/style";
+
+
+const PasswordCheckPage = () => {
+    return (
+        <MarginTopWrapper>
+            
+        </MarginTopWrapper>
+        );
+    }
+export default PasswordCheckPage;
