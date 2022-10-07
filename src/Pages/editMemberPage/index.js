46f99d1289a870f1627f8fa476824cc1122fb8b1
jjijju0918 <jisu7032@naver.com>
2022-10-07 12:50:07 +0900

    회원수정

--- src/Pages/editMemberPage/index.js
+++ src/Pages/editMemberPage/index.js
@@ -1,55 +1,44 @@
-import React from "react";
+import React,{ useState } from "react";
 import * as Styles from './style';
 import { MarginTopWrapper } from "../../Common/style";
 
 export const EditmemberPage = () => {
+    const [clicked, setClicked] = useState("Latest");
     return(
         <MarginTopWrapper margin>
-            <Styles.EditTitle>회원정보수정</Styles.EditTitle>
-            <Styles.TitleBar/>
-            <Styles.MemberInforBox>
-                <Styles.MemberContentBox>
-                   <Styles.MemberEdit>아이디</Styles.MemberEdit>
-                   <Styles.MemberConrent >jjijju</Styles.MemberConrent>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>비밀번호</Styles.MemberEdit>
-                    <Styles.Content placeholder="비밀번호를 입력해주세요." ></Styles.Content>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>비밀번호확인</Styles.MemberEdit>
-                    <Styles.Content placeholder="비밀번호를 다시입력해주세요." ></Styles.Content>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>이름</Styles.MemberEdit>
-                    <Styles.Content placeholder="김찌쭈" ></Styles.Content>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>이메일</Styles.MemberEdit>
-                    <Styles.Content placeholder="su70322@naver.com" ></Styles.Content>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>연락처</Styles.MemberEdit>
-                    <Styles.Content placeholder="010-5899-7032" ></Styles.Content>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>생년월일</Styles.MemberEdit>
-                    <Styles.Content placeholder="1997-09-18" ></Styles.Content>
-                </Styles.MemberContentBox>
-                <Styles.MemberContentBox>
-                    <Styles.MemberEdit>성별</Styles.MemberEdit>
-                    <Styles.GenderLabel htmlFor="m">남</Styles.GenderLabel>
-                    <Styles.GenderCheck type="radio" name="gender" id="m"/>
-                    <Styles.GenderLabel htmlFor="g">여</Styles.GenderLabel>
-                    <Styles.GenderCheck type="radio" name='gender' id="g"/>
-                </Styles.MemberContentBox>
-            </Styles.MemberInforBox>
-            <Styles.BtnBox>
-                <Styles.EditBtn>수정하기</Styles.EditBtn>
-                <Styles.EditBtn>이전</Styles.EditBtn>
-                <Styles.DeleteBtn>탈퇴하기</Styles.DeleteBtn>
-            </Styles.BtnBox>
-            
+            <Styles.EditTitle>나의 정보 관리</Styles.EditTitle>
+            <Styles.ProfileBox>
+                <Styles.LeftProfileBox>
+                    <Styles.ProfileImg src={"assets/임시프로필사진.png"}></Styles.ProfileImg>
+                    <Styles.MemberConrentBox>jjijju</Styles.MemberConrentBox>
+                    <Styles.Memberemail>su70322@naver.com</Styles.Memberemail>
+                    <Styles.TitleBar/>
+                    <Styles.LeftContent click={clicked === "Profile"} onClick={() => setClicked("Profile")}>회원수정</Styles.LeftContent>
+                    <Styles.LeftContent click={clicked === "Logout"} onClick={() => setClicked("Logout")} >로그아웃</Styles.LeftContent>
+                    <Styles.DeleteBtn>탈퇴하기 > </Styles.DeleteBtn>
+                </Styles.LeftProfileBox>
+                <Styles.MemberInforBox>
+                    <Styles.MemberContentBox>
+                        <Styles.MemberEdit>비밀번호</Styles.MemberEdit>
+                        <Styles.Content placeholder="비밀번호를 입력해주세요." ></Styles.Content>
+                    </Styles.MemberContentBox>
+                    <Styles.MemberContentBox>
+                        <Styles.MemberEdit>비밀번호확인</Styles.MemberEdit>
+                        <Styles.Content placeholder="비밀번호를 다시입력해주세요." ></Styles.Content>
+                    </Styles.MemberContentBox>
+                    <Styles.MemberContentBox>
+                        <Styles.MemberEdit>이름</Styles.MemberEdit>
+                        <Styles.Content placeholder="김찌쭈" ></Styles.Content>
+                    </Styles.MemberContentBox>
+                    <Styles.MemberContentBox>
+                        <Styles.MemberEdit>연락처</Styles.MemberEdit>
+                        <Styles.Content placeholder="010-5899-7032" ></Styles.Content>
+                    </Styles.MemberContentBox>
+                    <Styles.BtnBox>
+                        <Styles.EditBtn>수정하기</Styles.EditBtn>
+                    </Styles.BtnBox> 
+                </Styles.MemberInforBox>
+            </Styles.ProfileBox>
         </MarginTopWrapper>
     );
 }
--- src/Pages/editMemberPage/style.js
+++ src/Pages/editMemberPage/style.js
@@ -3,29 +3,66 @@ import styled from "styled-components";
 export const EditTitle = styled.div`
     font-size: 30px;
     font-weight: 500px;
-    margin-bottom: 15px;
-    margin-top: 200px;
-    text-align: center;
+    margin: 150px 200px 20px;
+    //text-align: center;
     font-weight: bold;
 `
 export const TitleBar = styled.div` 
-    width: 100%;
-    height: 2px;
-    margin-top: 10px;
-    margin-bottom: 30px;
-    background-color: black ;
-    //opacity: 0.2;
+    width: 170px;
+    height: 1px;
+    margin-left: 60px;
+    margin-top: 15px;
+    background-color: gray ;
+`
+export const ProfileBox = styled.div`
+    //width: 100%;
+    display: flex;
+    table-layout: fixed;
+    width: 800px;
+    //height: 100%; 
+    margin: 0 auto 200px ; 
+`
+export const LeftProfileBox = styled.div`
+    width: 300px;
+    background-color: #fff;
+    box-shadow: 5px 1px 8px 0 rgb(0 0 0 / 6%);
+    border-left: 1px solid rgba(0,0,0,.08);
+`
+export const LeftContent = styled.div` //클릭했을 때 찐하게 해놓기 자동 첫번째
+    width: 100px;
+    font-size: 19px;
+    margin: 40px 100px 30px;
+    text-align: center;
+    border-bottom: ${props => {
+        if(props.click) return "2px solid"
+        else return "none"
+    }};
+    
+    font-weight: ${props => {
+        if(props.click) return "bold"
+        else return "none"
+    }};
 `
 export const MemberInforBox = styled.div`
-    width: 900px;
-    height: 800px;
-    margin-left: 150px;
-    border-radius: 9px;
-    background-color: #E9E9E9;
+    height: 550px;
+    margin-left: 50px;
+    padding: 16px 17px 0;
+    border-radius: 15px;
+    box-shadow: 2px 2px 14px 0;
+    border: solid 1px rgba(54, 66, 69, 0.56);
+    background-color: #fff;
+    box-sizing: border-box;
+`
+export const ProfileImg = styled.img`
+    width: 112px;
+    height: 112px;
+    border-radius: 50px;
+    box-shadow: 0 3px 6px 0 rgb(29 34 53 / 8%);
+    margin: 100px 100px 0;
 `
 export const MemberContentBox = styled.div`
-    margin: 0 150px 0;
-    padding: 15px 130px;
+    margin: 10px 0 0;
+    padding: 15px 30px 10px;
 `
 export const MemberEdit = styled.div`
     width: 150px;
@@ -34,20 +71,16 @@ export const MemberEdit = styled.div`
     font-size: 15px;
     font-weight: bold;
 `
-export const MemberConrent = styled.div`
-    margin: 0;
-    border: 0;
-    outline: none;
+export const MemberConrentBox = styled.div`
+    margin-bottom: 10px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align:center;
+`
+export const Memberemail = styled.div`
+    text-align:center;
     font-size: 15px;
-    font-weight: 450;
-    display: block;
-    width: 350px;
-    height: 40px;
-    border-radius: 10px;
-    box-sizing: border-box;
-    padding: 0 0 0 9px;
-    background-color: #F3F3F3;
-    margin-top: 7px;
+    color: #929294;
 `
 export const Content = styled.input`
     margin: 0;
@@ -76,7 +109,7 @@ export const EditBtn = styled.button`
     border: 0;
     width: 100px;
     height: 40px;
-    margin: 30px 10px;
+    margin: 30px 150px;
     font-size: 15px;
     background: rgba(0, 100, 100, 0.5);
     color: white;
@@ -84,19 +117,15 @@ export const EditBtn = styled.button`
     cursor: pointer;
 `
 export const DeleteBtn = styled.button`
-    border: 0;
-    width: 100px;
-    height: 40px;
-    margin: 30px 10px;
-    font-size: 15px;
-    background-color: red;
-    color: white;
-    border-radius: 5px;
+    border: none;
+    margin-top: 70px;
+    margin-left: 200px;
+    background-color: white;
+    color: gray;
+    font-weight: bold;
+    font-size: 13px;
     cursor: pointer;
 `
 export const BtnBox = styled.div`
     width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 `
