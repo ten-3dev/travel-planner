f78ec9dc1034ad99afae33d40d53a24ed68408a4
jd <bamiyami@gmail.com>
2022-10-13 19:00:20 +0900

    8080 프록시 추가

---
+++ src/ApiService.js
@@ -0,0 +1,23 @@
+//import axios from "axios";
+
+//const USER_API_BASE_URL = "http://localhost:8080/users";
+
+// class ApiService {
+
+//     fetchUsers(){
+//         return axios.get(USER_API_BASE_URL);
+//     }
+//     fetchUsers(){
+//         return axios.get(USER_API_BASE_URL);
+//     }
+//     fetchUsers(){
+//         return axios.get(USER_API_BASE_URL);
+//     }
+//     fetchUsers(){
+//         return axios.get(USER_API_BASE_URL);
+//     }
+//     fetchUsers(){
+//         return axios.get(USER_API_BASE_URL);
+//     }
+
+//}
