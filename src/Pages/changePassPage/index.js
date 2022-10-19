1f2aacb0003abb545c7eff7c1f5d94fe72f1a26a
맵쥐 <gkdlfn@gmail.com>
2022-10-20 02:01:07 +0900

    수정중

--- src/Pages/changePassPage/index.js
+++ src/Pages/changePassPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Styles from './style';
 import { UserBlueBtn } from "../../Common/style";
 import { useLocation } from "react-router-dom";
@@ -7,24 +7,33 @@ import { useLocation } from "react-router-dom";
 const ChangePassPage = () => {
 
     const data = useLocation();
-    // const [pw, setPw] = useState("");
-    // const [pwRe, setPwRe] = useState("");
 
-     const { state }= data;
+    const [pw, setPw] = useState("");
+    const [pwRe, setPwRe] = useState("");
+    const { state }= data;
+    const ConfirmEmail = async () => {
+        try{
+            await axios.post('http://localhost:8080/checkEmail', {state,pw,pwRe});
+            
+            navigate('/');
+        }catch(e){
+            alert(e.response.data.msg);
+        }
+    }
+     
     return(
         <Styles.Wrapper>
             <Styles.ContentBox>
                 <Styles.LoginText>비밀번호 변경</Styles.LoginText>
-                <p>{state}</p>
                 <Styles.LoginText2>새 비밀번호
-                    <Styles.Input placeholder="새로운 비밀번호를 입력하세요"></Styles.Input>
+                    <Styles.Input placeholder="새로운 비밀번호를 입력하세요" onChange={(e) => setPw(e.target.value)}></Styles.Input>
                 </Styles.LoginText2>
 
                 <Styles.LoginText2>비밀번호 확인
-                        <Styles.Input placeholder="비밀번호를 다시 입력하세요"></Styles.Input>
+                        <Styles.Input placeholder="비밀번호를 다시 입력하세요"onChange={(e) => setPwRe(e.target.value)}></Styles.Input>
                 </Styles.LoginText2>
                 
-                <UserBlueBtn>비밀번호 변경</UserBlueBtn>
+                <UserBlueBtn  onClick={ConfirmEmail}>비밀번호 변경</UserBlueBtn>
             </Styles.ContentBox>
         </Styles.Wrapper>
     )
