462d518b1b9f5a699d202ca0824970639e6aab32
Daeyoung Gu <ocstar9@gmail.com>
2022-10-20 18:51:00 +0900

    travel수정

--- src/Pages/travelPage/index.js
+++ src/Pages/travelPage/index.js
@@ -63,7 +63,7 @@ const TravelPage = () => {
                     
             
                 // }
-                setTours();
+                setTours(filterTourItems);
             }
           })();
     }
