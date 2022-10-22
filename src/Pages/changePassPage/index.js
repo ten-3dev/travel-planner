eabbc4246e5c76751dbb38e7509d04e172c887aa
jung do lee <bamiyamy@gmail.com>
2022-10-23 04:19:02 +0900

    비번찾기수정중

--- src/Pages/changePassPage/index.js
+++ src/Pages/changePassPage/index.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import {useForm} from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import * as Styles from './style';
 import { UserBlueBtn } from "../../Common/style";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -24,47 +21,21 @@ const ChangePassPage = () => {
             alert(e.response.data.msg);
         }
     }
-    const schema = yup.object().shape({
-        pw: yup
-         .string()
-          .min(8, '비밀번호는 8자리 이상이어야 합니다.')
-          .max(25, '비밀번호는 25자리 이하여야 합니다.')
-          .matches(
-            /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-            "최소한개의 영문자, 숫자를 입력하세요."
-          )
-          .required('비밀번호를 입력해주세요.'),
-        checkPw: yup
-          .string()
-          .oneOf([yup.ref('pw'), null],
-          "비밀번호가 일치하지 않습니다.")
-          .required('비밀번호를 다시 입력해주세요.'),
-      });
-    
-      const { register, handleSubmit, formState:{ errors }} = useForm({
-         resolver: yupResolver(schema),
-        mode: 'onChange'});
-    
-      const onSubmit = (data) => console.log(data);
     
     return(
-        <Styles.Wrapper onSubmit={handleSubmit(onSubmit)}>
+        <Styles.Wrapper>
             <Styles.ContentBox>
                 <Styles.LoginText>비밀번호 변경</Styles.LoginText>
                 
-                <Styles.LoginText2 htmlFor="pw">새 비밀번호
-                    <Styles.Input type="password" placeholder="새로운 비밀번호를 입력하세요" {...register('pw')} onChange={(e) => setPw(e.target.value)}></Styles.Input>
-                    <Styles.ErrorMessage>{errors.pw && <Styles.ErrorMessage>{errors.pw.message}</Styles.ErrorMessage>}</Styles.ErrorMessage>
+                <Styles.LoginText2>새 비밀번호
+                    <Styles.Input type="password" placeholder="새로운 비밀번호를 입력하세요" ></Styles.Input>
                 </Styles.LoginText2>
 
-                <Styles.LoginText2 htmlFor="checkPw">비밀번호 확인
-                        <Styles.Input type="password" placeholder="비밀번호를 다시 입력하세요" {...register('checkPw')}></Styles.Input>
-                        <Styles.ErrorMessage>{errors.checkPw && <Styles.ErrorMessage>{errors.checkPw.message}</Styles.ErrorMessage>}</Styles.ErrorMessage>
+                <Styles.LoginText2>비밀번호 확인
+                        <Styles.Input type="password" placeholder="비밀번호를 다시 입력하세요"></Styles.Input>
                 </Styles.LoginText2>
                 
-                <UserBlueBtn type="submit" onClick={() => {
-                  PasswordChange();
-                  }}>비밀번호 변경</UserBlueBtn>
+                <UserBlueBtn>비밀번호 변경</UserBlueBtn>
             </Styles.ContentBox>
         </Styles.Wrapper>
     )
