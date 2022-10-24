4545dad6d4b1b4bab322df451433a1a6b46de2d2
mouse <gkdlfn032@gmail.com>
2022-10-24 16:52:45 +0900

    수정중

--- src/Pages/tourInfoPage/index.js
+++ src/Pages/tourInfoPage/index.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import * as Styles from './style';
 import { MarginTopWrapper } from "../../Common/style";
 // import {HeartOutlined, HeartFilled} from '@ant-design/icons';	
-import {useLocation } from "react-router-dom";
 import Map from "../../Components/kakaoMap";
-import Comment from "../../Components/Comment"
+import Paging from "../../Components/paging";
+import axios from "axios";
 
 
 const InformationPage = () => {
@@ -19,6 +19,25 @@ const InformationPage = () => {
             getTravelInfo(location.search.split("=")[1]);
         }
     }, [])
+
+    const [page, setPage] = useState(1); 
+    const [itemsCount] = useState(10);
+    const [totalItemsCount] = useState(0);
+    // const [email,setEmail] = useState("");
+    const [content,setContent] = useState("");
+    // const [name,setName] = useState("");
+    // const [day, setDay] = useState("");
+ 
+    
+    // useEffect (() => {
+
+    // })
+    // const getcontent = async () => { //조회 아직 ..
+    //     const data = await axios.get('http://localhost:8080/getComment');
+
+    // }
+    
+
 
     useEffect(() => {
         console.log(infoData);
@@ -35,6 +54,32 @@ const InformationPage = () => {
         return null;
     }
 
+    const type="T";
+    const writing = async (id) => { //등록아직...
+       if( window.confirm("등록하시겠습니까?")){
+        try{
+            await axios.post('http://localhost:8080/addComment',{id,content,type});
+            alert("댓글 추가 성공");
+
+        }catch(e){
+            alert(e.response.data.msg);
+        }
+    }
+    }
+
+
+    const [email, setEmail] = useState("");
+    useEffect(() => { // 제일 처음에 실행하는 애 
+        getData();
+       
+    },[])
+    const getData = async (id) => { // DB에 있는 회원데이터를 불러옴
+        const data = await axios.get('http://localhost:8080/getComment',{id});
+        
+        console.log(data);
+            
+    }
+    
     return(
         <MarginTopWrapper margin>
             <Styles.TitleBox>
@@ -78,7 +123,28 @@ const InformationPage = () => {
                     </Styles.DetaBox>
                 </Styles.DetailedInforBox>
             </Styles.InformationBox>
-            <Comment/>
+            <Styles.Comment1>
+                <Styles.Title1>톡톡</Styles.Title1>
+                <Styles.CommentBox>
+                    <Styles.ReviewBox>
+                            <Styles.ReImage src="assets/myProfile.png"/>
+                            <Styles.RefirstBox>
+                                <Styles.ReName>김지수</Styles.ReName>
+                                <Styles.ReDate>2022-09-18</Styles.ReDate>
+                                <Styles.ReContent>{content}</Styles.ReContent>   
+                            </Styles.RefirstBox>
+                    </Styles.ReviewBox>
+                    <Paging page={page} count={totalItemsCount} setPage={setPage} itemsCount={itemsCount}/>
+                    <Styles.InputBox>
+                        <Styles.ReviewTextBox>
+                            <Styles.ReviewText>리뷰남기기</Styles.ReviewText>
+                        </Styles.ReviewTextBox>
+                        <Styles.Profile1 src="assets/myProfile.png"/>
+                        <Styles.InputComment placeholder="댓글 입력" onChange={(e) => setContent(e.target.value)}/>
+                        <Styles.InputBtn onClick={() => { writing(location.search.split("=")[1])}}>등록</Styles.InputBtn>
+                    </Styles.InputBox>
+                </Styles.CommentBox>
+            </Styles.Comment1>
         </MarginTopWrapper>
     );
 }
