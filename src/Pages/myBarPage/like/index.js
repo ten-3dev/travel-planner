262c0e2a4d0857d1adca427b981c026f95b0293a
ten-3dev <mj03dev@gmail.com>
2022-10-25 01:59:56 +0900

    마이페이지 관광지 좋아요 ing...

--- src/Pages/myBarPage/like/index.js
+++ src/Pages/myBarPage/like/index.js
@@ -13,6 +13,7 @@ const Like = () => {
   const [itemsCount] = useState(6);
   const [totalItemsCount] = useState(50); // 임시
   const [like, setLike] = useState([]);
+  const [tourInfo, setTourInfo] = useState([]);
 
   useEffect(() => {
     console.log(page === 1 ? 1 : (page - 1) * itemsCount + "부터");
@@ -21,7 +22,14 @@ const Like = () => {
 
   useEffect(() => {
     getLikes();
+    const getDataTour = async () => {
+      await tourData();
+    }
+    getDataTour();
   }, [])
+
+  useEffect(() => {
+  }, [like])
 
   const getLikes = async () => {
     if(!sessionStorage.getItem("access_token")) return;
@@ -33,13 +41,21 @@ const Like = () => {
     }
   }
 
-  const tourData = async (props) =>{    //키워드 별 검색 함수
-    const response = await fetch(
-      `https://apis.data.go.kr/B551011/KorService/searchKeyword?serviceKey=${process.env.REACT_APP_TOUR_API_KEY}&numOfRows=100000&MobileOS=ETC&MobileApp=AppTest&_type=json&contentTypeId=12&listYN=Y&arrange=C&keyword=${props}`
-    );
-    const json = await response.json();
-    const tourItems = json.response.body.items.item;
-    console.log(tourItems);
+  const tourData = async () =>{    //키워드 별 검색 함수
+    if(like.length !== 0){
+      for(let i = 0; i < like.length; i++){
+        const response = await fetch(
+          `https://apis.data.go.kr/B551011/KorService/detailCommon?serviceKey=${process.env.REACT_APP_TOUR_API_KEY}&MobileOS=ETC&MobileApp=AppTest&_type=json&contentId=${like[i].id}&contentTypeId=12&defaultYN=Y&firstImageYN=Y&areacodeYN=N&catcodeYN=N&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y`
+        );
+        const json = await response.json();
+        const tourItems = json.response.body.items.item;
+        // setTourInfo(tourInfo.push({...tourItems[0], like: like[i]}))
+        const data = tourInfo;
+        data.push({...tourItems[0]});
+        setTourInfo(data);
+      }
+    }
+    console.log("tour: ", tourInfo);
 }
 
   return (
@@ -113,22 +129,19 @@ const Like = () => {
               <Styles.Text>관광지</Styles.Text>
             </Styles.Box>
             <Styles.SmallBox>
-              {like.map((el, idx) => {
-                
+              {tourInfo.map((el, idx) => { 
                 return(
                   <Styles.LineBox key={idx}>
                     <Styles.KeepBox3>
                       <Styles.ImgBox2 src={`assets/image32.png`}/>
                         <Styles.KeepBox>
                           <Styles.KeepBox2>
-                            <Styles.ContentText>더베이101</Styles.ContentText>
-                            <Styles.AddressText>부산 해운대구 동백로 52 더베이101</Styles.AddressText>
+                            <Styles.ContentText>{el?.title}</Styles.ContentText>
+                            <Styles.AddressText>{el?.addr1 + " " + el?.addr2}</Styles.AddressText>
                           </Styles.KeepBox2>
                           <Styles.KeepBox2>
                             <Styles.KeepContentText>
-                              부산 해운대를 대표하는해양레져 
-                              클럽하우스 화려한 야경과 맛있는 음식을 즐기는
-                              뭐라뭐라뭐라 쏼라쏼라쏼라 등등등등등등등이며 여러가지 뭐가 있을꺼임
+                              제목 클릭하여 상세정보 확인
                             </Styles.KeepContentText>
                             <Styles.KeepDeleteBox2>
                               <HeartFilled style={{ color: 'red', fontSize: '30px', cursor: "pointer"}}/>
